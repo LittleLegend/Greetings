1d52c0bd61fabed601bd740dc44a9328edaca740
Dominik Hirsch <D.hirsch98@gmx.net>
2018-06-06 00:59:43 +0200

    fixed

---
+++ Greetings/Greetings/Assets/InputController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class InputController{
+
+    Player Player;
+    Camera Camera;
+    public bool InputLocked;
+
+    public List<Gesture> GestureList;
+    public ICommand CheckInputCommand;
+
+    public InputController(Camera Camera, Player Player)
+    {
+        this.Camera = Camera;
+        this.Player = Player;
+    }
+
+    public void InputGreetingInput()
+    {
+        checkGreeting();
+        releaseGreeting();
+    }
+
+    public void checkGreeting()
+    {
+                for (int i = 0; i < GestureList.Count; i++)
+                {
+                    checkInput(GestureList[i]);
+                } 
+    }
+
+    public void releaseGreeting()
+    { 
+            if (Input.GetMouseButtonUp(0))
+            {
+                Player.greet();
+            
+            }
+    }
+    
+    public void checkInput(Gesture Gesture)
+    {
+        Gesture.Screenpoint = GetScreenTouchPoint();
+        CheckInputCommand = new CheckInputCommand(Gesture);
+        CheckInputCommand.execute();
+    }
+    
+    public Vector3 GetScreenTouchPoint()
+    {
+        Vector3 ScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        ScreenPoint = Camera.ScreenToViewportPoint(ScreenPoint);
+
+        return ScreenPoint;
+    }
+    
+    public void WatchSceneInput()
+    {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Player.closeDoor();
+                }
+    }
+}
---
+++ Greetings/Greetings/Assets/References.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class References : MonoBehaviour {
+
+    
+    public Player Player;
+
+    public Camera Camera;
+
+    public PeopleFactory PeopleFactory;
+    
+    public Animator Doorstep_Animator;
+    
+    public AnimationController AnimationController;
+
+    public InputController InputController;
+    
+    public DataManager DataManager;
+
+    public UIController UIController;
+
+    public StateMachine StateMachine;
+
+    public ScoreController ScoreController;
+
+    public GestureFactory GestureFactory;
+    
+    void Start () {
+
+        Player = new Player(StateMachine);
+        PeopleFactory = new PeopleFactory(DataManager);
+        GestureFactory = new GestureFactory(Player,StateMachine);
+        ScoreController = new ScoreController(UIController);
+        InputController = new InputController(Camera, Player);
+
+    }
+     
+}
